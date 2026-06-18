6bbe1b82a5acad39b48e3030ce8c2c3d8d990fad
Pedro-07 <pphenriquelim4@gmail.com>
2026-06-18 16:56:32 -0300

    fix: remove unsupported eslint key from next.config.ts for next 16 build fix

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.15.10', '192.168.15.32'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
